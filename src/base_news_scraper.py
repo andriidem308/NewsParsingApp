82e39cd24f9cbbe161c80d09617a797c1735edda
ademchenko <andrii.demchencko.c@pricespider.com>
2024-08-03 18:44:44 +0300

    upd

--- src/base_news_scraper.py
+++ src/base_news_scraper.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 import os.path
 import time
@@ -7,6 +8,7 @@ from urllib.request import urlretrieve
 
 from RPA.Browser.Selenium import Selenium
 from RPA.Excel.Files import Files
+from RPA.Robocorp.WorkItems import WorkItems
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.wait import WebDriverWait
@@ -31,12 +33,20 @@ class BaseNewsScraper(ABC):
 
     page_load_timeout = 5
 
-    def __init__(self, search_phrase: str, last_n_months: int = 0, category: str = None) -> None:
-        self.search_phrase = search_phrase
-        self.driver = Selenium()
-        self.category = category
+    def __init__(self) -> None:
+        self.work_items = WorkItems()
+        self.work_items.get_input_work_item()
+        work_items_input_data = self.work_items.get_work_item_variables()
 
+        search_phrase = work_items_input_data.get('search_phrase', '')
+        last_n_months = work_items_input_data.get('last_n_months', 0)
+        category = work_items_input_data.get('category')
+
+        self.search_phrase = search_phrase
+        self.category = category
         self.oldest_date = utils.get_oldest_date(last_n_months)
+
+        self.driver = Selenium()
 
         self.articles = []
 
@@ -49,37 +59,37 @@ class BaseNewsScraper(ABC):
         self.driver.set_window_size(self.viewport_x, self.viewport_y)
 
         self.driver.go_to(self.start_url)
-        self.logger.info(f'Start page {self.start_url} opened')
+        print(f'Start page {self.start_url} opened')
 
-        self.logger.info(f'Start searching articles via search input')
+        print(f'Start searching articles via search input')
         self.search()
 
-        self.logger.info(f'Sorting results (Newest first)')
+        print(f'Sorting results (Newest first)')
         self.sort_results_by_date()
-        self.logger.info(f'Results sorted!')
+        print(f'Results sorted!')
 
         if self.category is not None and self.has_category_filter:
-            self.logger.info(f'Filtering results by category "{self.category}"')
+            print(f'Filtering results by category "{self.category}"')
             self.filter_by_category()
-            self.logger.info(f'Results filtered!')
+            print(f'Results filtered!')
 
-        self.logger.info(f'Collecting articles')
+        print(f'Collecting articles')
         self.parse_page()
-        self.logger.info(f'Articles collected!')
+        print(f'Articles collected!')
 
-        self.logger.info(f'Trying to save results to the excel file')
-        self.save_articles_to_excel()
+        print(f'Trying to save results to the excel file')
+        self.save_articles()
 
     def create_dirs_if_not_exist(self) -> None:
         if not os.path.exists(OUTPUT_DIR):
-            self.logger.info(f'{OUTPUT_DIR} does not exist. Creating output directory...')
+            print(f'{OUTPUT_DIR} does not exist. Creating output directory...')
             os.mkdir(OUTPUT_DIR)
-            self.logger.info(f'Output directory created')
+            print(f'Output directory created')
 
         if not os.path.exists(PICTURES_DIR):
-            self.logger.info(f'{PICTURES_DIR} does not exist. Creating pictures directory...')
+            print(f'{PICTURES_DIR} does not exist. Creating pictures directory...')
             os.mkdir(PICTURES_DIR)
-            self.logger.info(f'Pictures directory created')
+            print(f'Pictures directory created')
 
     @utils.method_delay
     @abstractmethod
@@ -144,6 +154,8 @@ class BaseNewsScraper(ABC):
         raise NotImplementedError
 
     def process_article_data(self, article_data: Dict[str, Any]) -> Dict[str, Any]:
+        if isinstance(article_data['date'], datetime.date):
+            article_data['date'] = article_data['date'].isoformat()
         article_data['phrases_amount'] = utils.get_phrases_amount(self.search_phrase, article_data)
         article_data['contains_money'] = utils.check_money_noted(article_data)
         self.download_picture(article_data)
@@ -158,10 +170,10 @@ class BaseNewsScraper(ABC):
 
         try:
             urlretrieve(picture_url, picture_filepath)
-            self.logger.info(f'Picture {picture_filename} downloaded successfully')
+            print(f'Picture {picture_filename} downloaded successfully')
             article_data['picture'] = picture_filename
         except ValueError:
-            self.logger.info(f"Picture can't be downloaded via URI {picture_url}")
+            print(f"Picture can't be downloaded via URI {picture_url}")
 
     @utils.method_delay
     @abstractmethod
@@ -186,7 +198,9 @@ class BaseNewsScraper(ABC):
     def fill(self, xpath: str, text: str) -> None:
         self.driver.input_text(xpath, text)
 
-    def save_articles_to_excel(self) -> None:
+    def save_articles(self) -> None:
+        self.work_items.create_output_work_item({'articles': self.articles}, save=True)
+
         lib = Files()
         lib.create_workbook(path=f"output/{self.scraper_name}.xlsx", fmt="xlsx")
 
--- tasks.py
+++ tasks.py
@@ -1,9 +1,15 @@
+import json
+
 from robocorp.tasks import task
+from RPA.Robocorp.WorkItems import WorkItems
 
 from src.scrapers.latimes_scraper import LATimesScraper
+
+library = WorkItems()
 
 
 @task
 def main():
-    latimes_parser = LATimesScraper('coronavirus', last_n_months=3, category='California')
+    latimes_parser = LATimesScraper()
     latimes_parser.execute()
+
